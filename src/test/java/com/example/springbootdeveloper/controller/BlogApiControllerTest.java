3c629d88d69c2c47b992356f65b10888f0bb5db7
asasia1935 <sungmin1935@naver.com>
2024-02-21 04:58:15 +0900

    [Test] 블로그 글 전체 조회 API 실행 테스트 및 테스트 코드 작성

--- src/test/java/com/example/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/com/example/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -19,7 +19,9 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -70,5 +72,30 @@ class BlogApiControllerTest {
         assertThat(articles.size()).isEqualTo(1); // 크기가 1인지 검증
         assertThat(articles.get(0).getTitle()).isEqualTo(title);
         assertThat(articles.get(0).getContent()).isEqualTo(content);
+    }
+
+    @DisplayName("findAllArticles: 블로그 글 목록 조회에 성공한다.")
+    @Test
+    public void findAllArticles() throws Exception {
+        // given: 블로그 글 저장
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+
+        blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        // when: 목록 조회 API를 호출합니다.
+        final ResultActions resultActions = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then: 응답 코드가 200 OK이고, 반환 받은 값 중에 0번째 요소의 content와 title이 저장된 값과 같은지 확인
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value(content))
+                .andExpect(jsonPath("$[0].title").value(title));
+
     }
 }
