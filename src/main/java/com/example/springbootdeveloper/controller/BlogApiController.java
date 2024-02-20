7e3ce0757cfee87bd7d09f1352df566a13285fa4
asasia1935 <sungmin1935@naver.com>
2024-02-21 04:35:52 +0900

    [Feat] 블로그 글 전체 조회 API 구현

--- src/main/java/com/example/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/com/example/springbootdeveloper/controller/BlogApiController.java
@@ -2,14 +2,14 @@ package com.example.springbootdeveloper.controller;
 
 import com.example.springbootdeveloper.domain.Article;
 import com.example.springbootdeveloper.dto.AddArticleRequest;
+import com.example.springbootdeveloper.dto.ArticleResponse;
 import com.example.springbootdeveloper.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -24,5 +24,15 @@ public class BlogApiController {
 
         // 요청한 자원이 성공적으로 생성되었고 저장된 블로그 글 정보를 응답 객체에 담아 전송
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(articles);
     }
 }
--- src/main/java/com/example/springbootdeveloper/service/BlogService.java
+++ src/main/java/com/example/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,8 @@ import com.example.springbootdeveloper.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // 빈을 생성자로 생성 (final, @NotNull 키워드가 붙은 필드로 생성자 생성)
 @Service
 public class BlogService {
@@ -15,5 +17,10 @@ public class BlogService {
     // 블로그 글 추가 메서드
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 블로그 글 전체 조회 메서드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
