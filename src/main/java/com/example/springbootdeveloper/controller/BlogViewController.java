04e30784145223d5d9bdbaea2924eb6807df4c68
asasia1935 <sungmin1935@naver.com>
2024-02-25 22:55:10 +0900

    [Feat] 글 수정/생성 기능 코드 추가

--- src/main/java/com/example/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/com/example/springbootdeveloper/controller/BlogViewController.java
@@ -9,18 +9,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/articles")
 public class BlogViewController {
 
     private final BlogService blogService;
 
-    @GetMapping
+    @GetMapping("/articles")
     public String getArticles(Model model) {
         List<ArticleListViewResponse> articles = blogService.findAll().stream()
                 .map(ArticleListViewResponse::new)
@@ -31,11 +30,25 @@ public class BlogViewController {
         return "articleList";
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/articles/{id}")
     public String getArticle(@PathVariable Long id, Model model) {
         Article article = blogService.findById(id);
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    // id 키를 가진 쿼리파라미터의 값을 id 변수에 매핑(id가 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if(id == null) { // id가 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        }
+        else { // id가 없으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
