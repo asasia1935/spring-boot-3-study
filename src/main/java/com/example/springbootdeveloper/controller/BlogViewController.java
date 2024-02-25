baf3b1a6fe3dba8d2a7e78bb8d3dcb89bb8e500e
asasia1935 <sungmin1935@naver.com>
2024-02-25 19:39:48 +0900

    [Feat] 블로그 글 목록 뷰 구현

---
+++ src/main/java/com/example/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,28 @@
+package com.example.springbootdeveloper.controller;
+
+import com.example.springbootdeveloper.dto.ArticleListViewResponse;
+import com.example.springbootdeveloper.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+
+        return "articleList";
+    }
+}
