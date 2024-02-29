1508437c5b59ef76213c08619c4d13f541509aa6
asasia1935 <sungmin1935@naver.com>
2024-02-29 10:21:11 +0900

    [Feat] 회원가입 및 로그인 뷰 작성

--- src/main/java/com/example/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/com/example/springbootdeveloper/controller/UserApiController.java
@@ -2,8 +2,13 @@ package com.example.springbootdeveloper.controller;
 
 import com.example.springbootdeveloper.dto.AddUserRequest;
 import com.example.springbootdeveloper.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
