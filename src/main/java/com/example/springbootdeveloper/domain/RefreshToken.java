e27256e8c7c289b4acdec25e3d35e1d67b07364f
asasia1935 <sungmin1935@naver.com>
2024-03-30 22:51:45 +0900

    [Feat] 리프레시 토큰 도메인 구현

---
+++ src/main/java/com/example/springbootdeveloper/domain/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.example.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class RefreshToken { // DB에 저장하는 정보 -> 엔티티 추가
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private Long userId;
+
+    @Column(name = "refresh_token", nullable = false)
+    private String refreshToken;
+
+    public RefreshToken(Long userId, String refreshToken) {
+        this.userId = userId;
+        this.refreshToken = refreshToken;
+    }
+
+    public RefreshToken update(String newRefreshToken) {
+        this.refreshToken = newRefreshToken;
+        return this;
+    }
+}
