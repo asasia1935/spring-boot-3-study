f5fd7bd752cdd394a993133c0b938830e056a536
asasia1935 <sungmin1935@naver.com>
2024-04-09 23:43:24 +0900

    [Feat] 컨트롤러 추가(다시함)

--- src/main/java/com/example/springbootdeveloper/controller/TokenApiController.java
+++ src/main/java/com/example/springbootdeveloper/controller/TokenApiController.java
@@ -1,2 +1,27 @@
-package com.example.springbootdeveloper.controller;public class TokenApiController {
+package com.example.springbootdeveloper.controller;
+
+import com.example.springbootdeveloper.dto.CreateAccessTokenRequest;
+import com.example.springbootdeveloper.dto.CreateAccessTokenResponse;
+import com.example.springbootdeveloper.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token") // 요청이 오면
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        // 새 엑세스 토큰 생성
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
 }
--- src/main/java/com/example/springbootdeveloper/dto/CreateAccessTokenRequest.java
+++ src/main/java/com/example/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -1,2 +1,10 @@
-package com.example.springbootdeveloper.dto;public class CreateAccessTokenRequest {
+package com.example.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+    private String refreshToken;
 }
